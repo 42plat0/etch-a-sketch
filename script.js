5c3870695dd9868f0febb99831d54bea71bc7cd6
42plat0 <gytisnaujasis@icloud.com>
2024-01-31 03:29:04 +0200

    add working grid, can paint

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let gridContainer = document.querySelector(".gridContainer")
-let gridSize = 50;
+let gridSize = 100;
 
 //grid square size 
 let gridWidth = gridContainer.offsetWidth;
@@ -14,8 +14,41 @@ for (let i = 0; i < gridSize; i++){
     for(let j = 0; j < gridSize; j++){
         gridSquare = document.createElement('div');
         gridSquare.classList.add("gridSquare");
+        gridSquare.classList.add("selector");
+
         gridContainer.appendChild(gridSquare);
-        gridSquare.setAttribute("style",`flex-basis: ${squareWidth}px; height: ${squareHeight}`);
+        gridSquare.setAttribute("style",`flex-basis: ${squareWidth}px; height: ${squareHeight}; `);
     }
 }
+let gridArray = Array.from(document.querySelectorAll(".gridSquare"));
+let gridArrayLenght = gridArray.length;
+let isDown = false;
+
+for(let i = 0; i < gridArrayLenght; i++){
+    gridArray[i].setAttribute('id', i+1);
+
+    gridArray[i].addEventListener("mousedown", (e)=>{
+       
+        if(e.button == 0){
+            console.log(gridArray[i].id);
+            isDown = true;
+            gridArray[i].style.backgroundColor = "white";
+        }
+
+    })
+    gridArray[i].addEventListener("mouseup", ()=>{
+        isDown = false;
+    })
+    gridArray[i].addEventListener("mouseenter", ()=>{
+        if(isDown){
+            gridArray[i].style.backgroundColor = "white";
+        }
+    })
+}
+
+let playContainer = document.querySelector(".playContainer");
+playContainer.addEventListener("mouseleave", ()=>{
+    isDown = false;
+})
+
 
